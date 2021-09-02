fa72cacff7955cb06790d0c6e2220113edbf7dec
Thorne <sthorne@una.edu>
2021-09-02 13:38:28 -0500

    whater

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', function(req, res) {
 
-    res.send('<h3>Hello Node on Heroku</h3>');
+    res.send('<h3>Hello Node on Heroku</h3>'<a href="https://my-super-cool-awesome-project1.herokuapp.com/" /a>);
 
 });
 
